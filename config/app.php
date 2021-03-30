7dc7482c7c6c87eb4409ac23bbfcf159610e66a3
Mike Buonomo <mrb2590@gmail.com>
2021-03-30 23:31:41 +0000

    Update configs

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Recipes'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://recipes.buonomo.net'),
 
     'asset_url' => env('ASSET_URL', null),
 
